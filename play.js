8df51f3dd75e8cade3581f70393890338399a20d
Craig Anderson <cranderson0110@gmail.com>
2023-07-12 16:45:12 +1200

    Behind-the-scenes update to how the board is created and indexed
    Plus some preliminary work on movement

--- play.js
+++ play.js
@@ -3,7 +3,6 @@ window.onload = () => {
 	setBoard();
 };
 
-
 const whiteKing = "♔";
 const whiteQueen = "♕";
 const whiteRook = "♖";
@@ -20,26 +19,63 @@ const blackKnight = "♞";
 const blackPawn = "♟︎";
 const blackPieces = [blackKing, blackQueen, blackRook, blackBishop, blackKnight, blackPawn];
 
+
+var selectedRow = null;
+var selectedColumn = null;
+
+var possible = [];
+
 function createBoard() {
 	board = document.getElementsByTagName("board")[0];
-	id = 1;
-	for (let i = 1; i < 22; i++) {
+	document.body.onclick = (e) => {
+		deselect();
+	}
+	for (let r = 1; r < 22; r++) {
 		row = document.createElement("row");
-		if (i < 6)
-			n = i;
-		else if (i < 17) {
-			if (i % 2)
+		if (r < 6) {
+			n = r;
+			m = Math.min(4 - n + (r + 1) % 2, 2);
+		} else if (r < 17) {
+			if (r % 2)
 				n = 5;
 			else
 				n = 6;
+			m = 0;
 		} else {
-			n = 22 - i;
+			n = 22 - r;
+			m = Math.min(4 - n + (r + 1) % 2, 2);
 		}
-		for (let h = 0; h < n; h++) {
+
+		for (let h = 0; h < 6 - r % 2; h++) {
 			hex = document.createElement("hex");
+			if (h < m) {
+				hex.classList.add("disabled");
+			} else if (m <= h && h < 6 - m - r % 2) {
+				switch (r % 3) {
+					case 0:
+						hex.classList.add("tile1");
+						break;
+					case 1:
+						hex.classList.add("tile2");
+						break;
+					case 2:
+						hex.classList.add("tile3");
+						break;
+				}
+				hex.onclick = (e) => {
+					if (selectSquare(r - 1, h))
+						showPossible(r - 1, h);
+					// event is handled, stop propagation now
+					var evt = e ? e : window.event;
+					if (evt.stopPropagation) { evt.stopPropagation(); }
+					else { evt.cancelBubble = true; }
+					return false;
+				};
+			} else {
+				hex.classList.add("disabled");
+			}
 			p = document.createElement("p");
 			hex.appendChild(p);
-			id++;
 			row.appendChild(hex);
 		}
 		board.appendChild(row);
@@ -47,14 +83,14 @@ function createBoard() {
 }
 
 function setBoard() {
-	setPieceRaw(0, 0, blackBishop);
-	setPieceRaw(1, 0, blackQueen);
-	setPieceRaw(1, 1, blackKing);
-	setPieceRaw(2, 0, blackKnight);
-	setPieceRaw(2, 1, blackBishop);
-	setPieceRaw(2, 2, blackKnight);
-	setPieceRaw(3, 0, blackRook);
-	setPieceRaw(3, 3, blackRook);
+	setPieceRaw(0, 2, blackBishop);
+	setPieceRaw(1, 2, blackQueen);
+	setPieceRaw(1, 3, blackKing);
+	setPieceRaw(2, 1, blackKnight);
+	setPieceRaw(2, 2, blackBishop);
+	setPieceRaw(2, 3, blackKnight);
+	setPieceRaw(3, 1, blackRook);
+	setPieceRaw(3, 4, blackRook);
 	setPieceRaw(4, 2, blackBishop);
 	setPieceRaw(4, 0, blackPawn);
 	setPieceRaw(4, 4, blackPawn);
@@ -66,14 +102,14 @@ function setBoard() {
 	setPieceRaw(7, 3, blackPawn);
 	setPieceRaw(8, 2, blackPawn);
 
-	setPieceRaw(20, 0, whiteBishop);
-	setPieceRaw(19, 0, whiteQueen);
-	setPieceRaw(19, 1, whiteKing);
-	setPieceRaw(18, 0, whiteKnight);
-	setPieceRaw(18, 1, whiteBishop);
-	setPieceRaw(18, 2, whiteKnight);
-	setPieceRaw(17, 0, whiteRook);
-	setPieceRaw(17, 3, whiteRook);
+	setPieceRaw(20, 2, whiteBishop);
+	setPieceRaw(19, 2, whiteQueen);
+	setPieceRaw(19, 3, whiteKing);
+	setPieceRaw(18, 1, whiteKnight);
+	setPieceRaw(18, 2, whiteBishop);
+	setPieceRaw(18, 3, whiteKnight);
+	setPieceRaw(17, 1, whiteRook);
+	setPieceRaw(17, 4, whiteRook);
 	setPieceRaw(16, 2, whiteBishop);
 	setPieceRaw(16, 0, whitePawn);
 	setPieceRaw(16, 4, whitePawn);
@@ -97,4 +133,121 @@ function setPieceRaw(row, column, piece) {
 		p.classList.add("black");
 	else if (whitePieces.includes(piece))
 		p.classList.add("white");
+}
+
+
+function deselect() {
+	applyHighlight();
+	possible = [];
+	board = document.getElementsByTagName("board")[0];
+	if (selectedRow != null && selectedColumn != null) {
+		r = board.children[selectedRow];
+		h = r.children[selectedColumn];
+		h.classList.remove("selected")
+		selectedRow = null;
+		selectedColumn = null;
+	}
+}
+
+function selectSquare(row, column) {
+	board = document.getElementsByTagName("board")[0];
+	deselect();
+	r = board.children[row];
+	h = r.children[column];
+	p = h.children[0];
+	piece = p.textContent;
+	if (whitePieces.includes(piece)) {
+		selectedRow = row;
+		selectedColumn = column;
+		h.classList.add("selected")
+		return true;
+	}
+	return false;
+}
+
+function highlight(row, column) {
+	board = document.getElementsByTagName("board")[0];
+	r = board.children[row];
+	if (r == undefined)
+		return;
+	h = r.children[column];
+	if (h == undefined)
+		return;
+	h.classList.toggle("possible");
+}
+
+function applyHighlight() {
+	possible.forEach(element => {
+		highlight(element[0], element[1]);
+	});
+}
+
+function showPossible(row, column) {
+	applyHighlight();
+	possible = [];
+	board = document.getElementsByTagName("board")[0];
+	r = board.children[row];
+	h = r.children[column];
+	p = h.children[0];
+	piece = p.textContent;
+
+	offset = row % 2;
+	switch (piece) {
+		case whitePawn:
+			// starting row (white)
+			if (11 < row && row < 17) {
+				possible.push([row - 4, column]);
+			}
+			possible.push([row - 2, column]);
+			break;
+		case whiteKing:
+			// forward
+			possible.push([row - 2, column]);
+			// left
+			possible.push([row, column - 1]);
+			// left up
+			possible.push([row - 1, column - offset]);
+			// forward left
+			possible.push([row - 3, column - offset]);
+			// right
+			possible.push([row, column + 1]);
+			// right up
+			possible.push([row - 1, column + 1 - offset]);
+			// forward right
+			possible.push([row - 3, column + 1 - offset]);
+			// left down
+			possible.push([row + 1, column - offset]);
+			// backward left
+			possible.push([row + 3, column - offset]);
+			// right down
+			possible.push([row + 3, column + 1 - offset]);
+			// backward right
+			possible.push([row + 1, column + 1 - offset]);
+			// backward
+			possible.push([row + 2, column]);
+			break;
+		case whiteKnight:
+			// forward
+			possible.push([row - 5, column - offset]);
+			possible.push([row - 5, column + 1 - offset]);
+			// forward left
+			possible.push([row - 4, column - 1]);
+			possible.push([row - 1, column - 1 - offset]);
+			// forward right
+			possible.push([row - 4, column + 1]);
+			possible.push([row - 1, column + 2 - offset]);
+			// backward left
+			possible.push([row + 1, column - 1 - offset]);
+			possible.push([row + 4, column - 1]);
+			// backward right
+			possible.push([row + 1, column + 2 - offset]);
+			possible.push([row + 4, column + 1]);
+			// backward
+			possible.push([row + 5, column - offset]);
+			possible.push([row + 5, column + 1 - offset]);
+			break;
+		default:
+			break;
+	}
+	applyHighlight();
 }
